d4130416caabca9b00ac0420f0fd5d9dd21e5065
Keertiraj Patil <keerti4p@gmail.com>
2023-04-03 20:52:47 +0200

    add

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import boto3
 session = boto3.Session(
-    aws_access_key_id='assdsdasdsadasfas',,
+    aws_access_key_id='assdsdasdsadasfas',
     aws_secret_access_key='fegfdhtyhdyhh'
 )
+
+AWS_SECRET_ACCESS_KEY = '*********'
